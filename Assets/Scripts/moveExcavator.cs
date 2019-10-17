be41ec7eef4733640180977501e43ea460f43a67
CesarEstrada15 <43758711+CesarEstrada15@users.noreply.github.com>
2019-10-17 10:26:12 -0600

    First commit

---
+++ Assets/Scripts/moveExcavator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveExcavator : MonoBehaviour
+{
+    int speed = 10;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") *  speed * Time.deltaTime, 0f);
+    }
+}
